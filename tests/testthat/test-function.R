e055752455820aa3ec42322354922688f1779171
Prashant Biradar <prashant.biradar@gmail.com>
2021-03-07 13:19:23 +0530

    Update test-function.R

--- tests/testthat/test-function.R
+++ tests/testthat/test-function.R
@@ -3,11 +3,12 @@ context("If JSON convert it in to list")
 with_mock_api({
   test_that("are we getting JSOn response", {
     response <- getData("x")
+    expect_is(response, "application/json")
   })
 })
 
 test_that("If JSON convert it in to list", {
-  user <- getData("x")
-  expect_is(response, "application/json")
+  response <- getData("x")
+  expect_is(response, "application/list")
   expect_output(print(response), "x")
 })
